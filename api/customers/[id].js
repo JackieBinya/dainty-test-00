ecfb5f47cafa24288cc11d55bd9f4ef237282000
JackieBinya <jacquelinebinya@gmail.com>
2021-03-18 18:11:05 +0200

    Catch

--- api/customers/[id].js
+++ api/customers/[id].js
@@ -1,9 +1,13 @@
 const stripe = require("stripe")(process.env.STRIPE_SK)
 
 module.exports = async (req, res) => {
-  const customer = await stripe.customers.update(
-    req.query.id,
-    { email: req.body.email },
-    { description: "User initially subbed on Free Trial" }
-  )
+  try {
+    const customer = await stripe.customers.update(
+      req.query.id,
+      { email: req.body.email },
+      { description: "User initially subbed on Free Trial" }
+    )
+  } catch (error) {
+    console.log(error)
+  }
 }
