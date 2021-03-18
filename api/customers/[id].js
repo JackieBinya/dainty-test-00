4aae2607d3d3195ee8f5b1b3b89021846da8fe9d
JackieBinya <jacquelinebinya@gmail.com>
2021-03-18 18:28:37 +0200

    Yess

--- api/customers/[id].js
+++ api/customers/[id].js
@@ -2,6 +2,10 @@ const stripe = require("stripe")(process.env.STRIPE_SK)
 
 module.exports = async (req, res) => {
   try {
+    console.log("******************************")
+    console.log(req.query.id)
+    console.log(req.body.email)
+    console.log("******************************")
     const customer = await stripe.customers.update(
       req.query.id,
       { email: req.body.email },
