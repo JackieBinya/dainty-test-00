419d5503c5f991b560cd512a280d2944623d5486
JackieBinya <jacquelinebinya@gmail.com>
2021-03-18 18:46:19 +0200

    dddd fff

--- api/customers/[id].js
+++ api/customers/[id].js
@@ -5,10 +5,11 @@ module.exports = async (req, res) => {
     console.log("******************************")
     console.log(req.query.id)
     console.log(req.body.email)
-    const customer = await stripe.customers.update(req.query.id, [
-      { email: req.body.email },
-      { description: "User initially subbed on Free Trial" },
-    ])
+    console.log("******************************")
+    const customer = await stripe.customers.update(req.query.id, {
+      email: req.body.email,
+      description: "User initially subbed on Free Trial",
+    })
   } catch (error) {
     console.log(error)
   }
