38e0c86a7d5c7b11bdca1e88c3fbca6457397ee4
JackieBinya <jacquelinebinya@gmail.com>
2021-03-18 15:07:34 +0200

    Update a user's decsription

--- api/customers/[id].js
+++ api/customers/[id].js
@@ -1,5 +1,9 @@
 const stripe = require("stripe")(process.env.STRIPE_SK)
 
 module.exports = async (req, res) => {
-  const customer = await stripe.customers.update(req.query.id, { email: req.body.email })
+  const customer = await stripe.customers.update(
+    req.query.id,
+    { email: req.body.email },
+    { description: "User initially subbed on Free Trial" }
+  )
 }
