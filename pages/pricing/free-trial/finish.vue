397b8814557376193917bf4638dbeaed040ad22e
JackieBinya <jacquelinebinya@gmail.com>
2021-03-19 04:34:15 +0200

    Change styles

--- pages/pricing/free-trial/finish.vue
+++ pages/pricing/free-trial/finish.vue
@@ -1,21 +1,28 @@
 <template>
-  <div class="free-trial">
-    <div>
-      <div class="free-trial_inner">
-        <div class="free-trial_action">
-          <div>
-            <form @submit.prevent="handleSubmit">
-              <input type="email" placeholder="Work Email*" :value="email" />
-              <div id="card-element">
-                <!-- A Stripe card Element will be inserted here. -->
-              </div>
-              <div id="card-errors" role="alert"></div>
-              <input type="submit" value="Link your card" />
-            </form>
-            <div class="sr-result hidden">
-              <p>Card setup completed<br /></p>
+  <div class="sr-root">
+    <div class="sr-main">
+      <div>
+        <form @submit.prevent="handleSubmit" class="sr-payment-form card">
+          <div class="sr-form-row">
+            <label> Account details </label>
+            <input type="email" placeholder="Work Email*" :value="email" />
+          </div>
+
+          <div class="sr-form-row">
+            <label> Payment details </label>
+            <div id="card-element">
+              <!-- A Stripe card Element will be inserted here. -->
             </div>
           </div>
+          <div class="sr-field-error" id="card-errors" role="alert"></div>
+          <!-- <input type="submit" value="Link your card" /> -->
+          <button type="submit">
+            <div class="spinner hidden" id="spinner"></div>
+            <span id="button-text">Link your card to your account</span>
+          </button>
+        </form>
+        <div class="sr-result hidden">
+          <p>Card setup completed<br /></p>
         </div>
       </div>
     </div>
@@ -85,7 +92,7 @@ export default {
 
         this.$store.commit("updateEmail", "")
 
-         this.$store.commit("updateSetupIntent", {})
+        this.$store.commit("updateSetupIntent", {})
       }
     },
     subscribeFreeTrial({ customer }) {
@@ -96,8 +103,8 @@ export default {
         },
         body: JSON.stringify({ customer }),
       })
-    }
-  }
+    },
+  },
 }
 </script>
 
@@ -112,49 +119,310 @@ export default {
     padding: 3rem 0;
   }
 }
-
-.free-trial_inner {
-  margin-top: 5rem;
+sr-root {
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  max-width: 980px;
+  padding: 48px;
+  align-content: center;
+  justify-content: center;
+  height: auto;
+  min-height: 100vh;
+  margin: 0 auto;
+}
+.sr-main {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 100%;
+  align-self: center;
+  padding: 50px 50px 75px 50px;
+  background: #fff;
+  width: 400px;
+  border-radius: 6px;
+  box-shadow: 0px 0px 0px 0.5px rgba(50, 50, 93, 0.1), 0px 2px 5px 0px rgba(50, 50, 93, 0.1),
+    0px 1px 1.5px 0px rgba(0, 0, 0, 0.07);
+}
+.sr-picker {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 25px;
+}
+.sr-picker button {
+  background: none !important;
+  border: none;
+  padding: 0 !important;
+  /*optional*/
+  font-family: arial, sans-serif;
+  /*input has OS specific font-family*/
+  color: var(--acc-purple-color);
+  cursor: pointer;
+  width: 75px;
+  box-shadow: none;
+  border-radius: 0;
+}
+.sr-picker button:hover,
+.sr-picker button:focus,
+.sr-picker button.selected {
+  outline: none;
+  box-shadow: none;
+  border-radius: 0;
+}
+.sr-picker button:hover,
+.sr-picker button:focus {
+  border-bottom: 2px solid #ed5f74;
+}
+.sr-picker button.selected {
+  border-bottom: 2px solid var(--acc-purple-color);
+}
+.sr-field-error {
+  color: rgb(105, 115, 134);
+  text-align: left;
+  font-size: 13px;
+  line-height: 17px;
+  margin-top: 12px;
 }
 
-.free-trial_action {
-  h2 {
-    margin-bottom: 20px;
-  }
-  p {
-    font-size: 20px !important;
-    line-height: 1.875;
-    letter-spacing: -0.005em;
-    // color: rgb(49, 67, 81);
-    margin-bottom: 30px;
-    max-width: 642px;
+/* Inputs */
+.sr-input,
+input[type="text"] {
+  border: 1px solid rgba(0, 0, 0, 0.15);
+  border-radius: 6px;
+  padding: 5px 12px;
+  height: 44px;
+  width: 100%;
+  transition: box-shadow 0.2s ease;
+  background: white;
+  -moz-appearance: none;
+  -webkit-appearance: none;
+  appearance: none;
+}
+.sr-input:focus,
+input[type="text"]:focus,
+button:focus,
+.focused {
+  box-shadow: 0 0 0 1px rgba(50, 151, 211, 0.3), 0 1px 1px 0 rgba(0, 0, 0, 0.07),
+    0 0 0 4px rgba(50, 151, 211, 0.3);
+  outline: none;
+  z-index: 9;
+}
+.sr-input::placeholder,
+input[type="text"]::placeholder {
+  color: rgba(0, 0, 0, 0.4);
+}
+.sr-result {
+  height: 44px;
+  -webkit-transition: height 1s ease;
+  -moz-transition: height 1s ease;
+  -o-transition: height 1s ease;
+  transition: height 1s ease;
+  color: rgb(105, 115, 134);
+  overflow: auto;
+}
+.sr-result code {
+  overflow: scroll;
+}
+.sr-result.expand {
+  height: 350px;
+}
+
+.sr-combo-inputs-row {
+  box-shadow: 0px 0px 0px 0.5px rgba(50, 50, 93, 0.1), 0px 2px 5px 0px rgba(50, 50, 93, 0.1),
+    0px 1px 1.5px 0px rgba(0, 0, 0, 0.07);
+  border-radius: 7px;
+}
+
+/* Form */
+.sr-form-row {
+  margin: 16px 0;
+}
+label {
+  font-size: 13px;
+  font-weight: 500;
+  margin-bottom: 8px;
+  display: inline-block;
+}
+
+/* Buttons and links */
+button {
+  background: var(--acc-purple-color);
+  border-radius: 6px;
+  color: white;
+  border: 0;
+  padding: 12px 16px;
+  margin-top: 16px;
+  font-weight: 600;
+  cursor: pointer;
+  transition: all 0.2s ease;
+  display: block;
+  box-shadow: 0px 4px 5.5px 0px rgba(0, 0, 0, 0.07);
+  width: 100%;
+}
+button:hover {
+  filter: contrast(115%);
+}
+button:active {
+  transform: translateY(0px) scale(0.98);
+  filter: brightness(0.9);
+}
+button:disabled {
+  opacity: 0.5;
+  cursor: none;
+}
+
+a {
+  color: #fff;
+  text-decoration: none;
+  transition: all 0.2s ease;
+}
+
+a:hover {
+  filter: brightness(0.8);
+}
+
+a:active {
+  filter: brightness(0.5);
+}
+
+/* Stripe Element placeholder */
+.sr-card-element {
+  padding-top: 12px;
+}
+
+/* Responsiveness */
+@media (max-width: 720px) {
+  .sr-root {
+    flex-direction: column;
+    justify-content: flex-start;
+    padding: 48px 20px;
+    min-width: 320px;
   }
 
-  input {
-    height: 52px;
-    line-height: 24px;
+  .sr-header__logo {
+    background-position: center;
   }
 
-  input[type="email"] {
-    border: 1px solid rgb(165, 178, 189);
-    border-radius: 4px 0 0 4px;
-    width: 365px;
-    padding: 0 0 12px 12px;
-    color: #253642;
-    outline: 0;
-    font-size: 15px;
+  .sr-payment-summary {
+    text-align: center;
   }
 
-  input[type="submit"] {
-    position: absolute;
-    border: 1px solid var(--acc-purple-color);
-    border-radius: 0 4px 4px 0;
-    padding: 0 20px 0 20px;
-    font-size: 18px;
-    color: white;
-    background: var(--acc-purple-color);
-    font-weight: 500;
-    cursor: pointer;
+  .sr-content {
+    display: none;
+  }
+
+  .sr-main {
+    width: 100%;
+    height: 305px;
+    background: rgb(247, 250, 252);
+    box-shadow: 0px 0px 0px 0.5px rgba(50, 50, 93, 0.1), 0px 2px 5px 0px rgba(50, 50, 93, 0.1),
+      0px 1px 1.5px 0px rgba(0, 0, 0, 0.07);
+    border-radius: 6px;
+  }
+}
+
+/* todo: spinner/processing state, errors, animations */
+
+.spinner,
+.spinner:before,
+.spinner:after {
+  border-radius: 50%;
+}
+.spinner {
+  color: #ffffff;
+  font-size: 22px;
+  text-indent: -99999px;
+  margin: 0px auto;
+  position: relative;
+  width: 20px;
+  height: 20px;
+  box-shadow: inset 0 0 0 2px;
+  -webkit-transform: translateZ(0);
+  -ms-transform: translateZ(0);
+  transform: translateZ(0);
+}
+.spinner:before,
+.spinner:after {
+  position: absolute;
+  content: "";
+}
+.spinner:before {
+  width: 10.4px;
+  height: 20.4px;
+  background: var(--acc-purple-color);
+  border-radius: 20.4px 0 0 20.4px;
+  top: -0.2px;
+  left: -0.2px;
+  -webkit-transform-origin: 10.4px 10.2px;
+  transform-origin: 10.4px 10.2px;
+  -webkit-animation: loading 2s infinite ease 1.5s;
+  animation: loading 2s infinite ease 1.5s;
+}
+.spinner:after {
+  width: 10.4px;
+  height: 10.2px;
+  background: var(--acc-purple-color);
+  border-radius: 0 10.2px 10.2px 0;
+  top: -0.1px;
+  left: 10.2px;
+  -webkit-transform-origin: 0px 10.2px;
+  transform-origin: 0px 10.2px;
+  -webkit-animation: loading 2s infinite ease;
+  animation: loading 2s infinite ease;
+}
+
+@-webkit-keyframes loading {
+  0% {
+    -webkit-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+@keyframes loading {
+  0% {
+    -webkit-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+
+/* Animated form */
+
+.sr-root {
+  animation: 0.4s form-in;
+  animation-fill-mode: both;
+  animation-timing-function: ease;
+}
+
+.hidden {
+  display: none;
+}
+
+@keyframes field-in {
+  0% {
+    opacity: 0;
+    transform: translateY(8px) scale(0.95);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0px) scale(1);
+  }
+}
+
+@keyframes form-in {
+  0% {
+    opacity: 0;
+    transform: scale(0.98);
+  }
+  100% {
+    opacity: 1;
+    transform: scale(1);
   }
 }
 </style>
