7d1bf1e92415cf78955083b9c85a72c7995546fe
JackieBinya <jacquelinebinya@gmail.com>
2021-03-20 08:01:46 +0200

    Update styles

--- pages/pricing/free-trial/index.vue
+++ pages/pricing/free-trial/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="free-trial">
+    <h2>Core Plan - 15 Day Free Trial</h2>
     <div class="free-trial_inner">
       <div class="free-trial_content-wrapper">
         <div class="free-trial_content">
@@ -16,12 +17,20 @@
       </div>
 
       <div class="free-trial_content-side">
-        <h3>Core Plan</h3>
         <ul>
-          <li v-for="service in services" :key="service.length">
-            <check-icon />
-            <span> {{ service }}</span>
-          </li>
+          <div>
+            <li v-for="service in services.slice(0, 9)" :key="service.length">
+              <check-icon />
+              <span> {{ service }}</span>
+            </li>
+          </div>
+
+          <div>
+            <li v-for="service in services.slice(9)" :key="service.length">
+              <check-icon />
+              <span> {{ service }}</span>
+            </li>
+          </div>
         </ul>
       </div>
     </div>
@@ -32,7 +41,7 @@
 import checkIcon from "../../../components/home/plans/check-icon.vue"
 export default {
   components: { checkIcon },
-  layout: "stripe-payment-method-capture-layout",
+  // layout: "stripe-payment-method-capture-layout",
   data() {
     return {
       fullname: "",
@@ -43,6 +52,17 @@ export default {
         "Social Media Posts",
         "Advertisements",
         "Logo Design",
+        "Business Card Design",
+        "Letterhead Design",
+        "Stationary",
+        "E-Book Cover",
+        "Infographic",
+        "Flyer",
+        "Brochure",
+        "Packaging",
+        "T-shirt",
+        "and more...",
+        "No contracts - cancel anytime",
       ],
     }
   },
@@ -79,28 +99,37 @@ export default {
 <style lang="scss" scoped>
 .free-trial {
   width: 100%;
-  max-width: 1140px;
+  max-width: 1240px;
   margin: 0 auto;
+  padding-top: 9rem;
+
+  h2 {
+    text-align: center;
+  }
 }
 
 .free-trial_inner {
   margin-top: 3rem;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 1.5rem;
 }
 
 .free-trial_content-wrapper {
-  position: relative;
-  margin: 0 auto;
-  width: 625px;
-  background: white;
+  border-top: 1px solid #d5d5d5;
+  // position: relative;
+  // margin: 0 auto;
+  // width: 625px;
+  // background: white;
 }
 
 .setup-intent-form {
-  padding: 60px 70px;
-  border-radius: 4px;
-  box-shadow: 0 5px 30px 0 rgba(39, 63, 74, 0.15);
-  min-height: 382px;
+  // padding: 30px 70px 60px;
+  // border-radius: 4px;
+  // box-shadow: 0 5px 30px 0 rgba(39, 63, 74, 0.15);
+  // min-height: 382px;
   // padding: 40px 30px;
-  position: relative;
+  // position: relative;
 
   h1 {
     font-size: 23px;
@@ -116,20 +145,21 @@ export default {
     height: 50px;
     padding: 0 1rem;
     margin: 1rem 0;
-    border-radius: 3px;
     font-size: 14px;
   }
 
   input[type="email"],
   input[type="text"] {
     border: 1px solid rgb(165, 178, 189);
+    border-radius: 3px;
   }
 
   input[type="submit"] {
     border: 2px solid var(--acc-purple-color);
+    border-radius: 8px;
     color: var(--acc-purple-color);
     font-weight: 500;
-    font-size: 16px;
+    font-size: 14px;
     cursor: pointer;
 
     &:hover {
@@ -146,12 +176,20 @@ export default {
 }
 
 .free-trial_content-side {
-  position: absolute;
-  right: 30px;
-  top: 192px;
+  border-top: 6px solid var(--acc-purple-color);
+  padding: 30px 70px 60px;
+  // border-radius: 4px;
+  box-shadow: 0 5px 30px 0 rgba(39, 63, 74, 0.15);
+  // position: absolute;
+  // right: 30px;
+  // top: 192px;
+  // ul {
+  //   display: flex;
+  //   justify-content: space-between;
+  // }
 
   h3 {
-    font-size: 18px;
+    font-size: 21px;
     // @apply text-accentPurple;
   }
 
